a313cf5ecf100c6509f0c3defac851834a11ea8d
Tamami Polk <tamami_polk@hotmail.com>
2019-03-29 13:11:31 -0500

    version 3 added logic to click on character

---
+++ assets/javascript/game.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+
+    var playerCharacter;
+
+   // check to xee if the first player was chosen for the character
+    $("#row-one-player-1").on("click", function () {
+
+        playerCharacter = "playerOne"
+
+    } );
+
+
+    $("#row-one-player-2").on("click", function () {
+
+        playerCharacter = "playerTwo"
+    } );
+
+    $("#row-one-player-3").on("click", function () {
+
+        playerCharacter = "playerThree"
+     } );
+
+     $("#row-one-player-4").on("click", function () {
+
+        playerCharacter = "playerFour"
+    } );
+
+
+
+
+
+
+
+
+
+    }
+);
+
